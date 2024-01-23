21bf3b17d050165c40b2ab12408d7290a208fef5
Alexey Pavlenko <sk.alexey.pavlenko@gmail.com>
2024-01-23 20:52:53 +0200

    badge

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -60,7 +60,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> My-Buddy.AI</span>
       </a>
     </span>
   );
